a32886b93499e77ae61d0f24bc73a741237242a4
benoit <maliarbenoit@msn.com>
2014-04-07 17:36:02 +0200

    Commenting the graph's file
    Signed-off-by: benoit <maliarbenoit@msn.com>

--- Web/search.php
+++ Web/search.php
@@ -31,6 +31,8 @@
         <script type="text/javascript" src="/ink/js/ink-ui.min.js"></script>
         <script type="text/javascript" src="/ink/js/autoload.js"></script>
         <script type="text/javascript" src="/ink/js/html5shiv.js"></script>
+        <script type="text/javascript" src="Chart.js"></script>
+
         
         
         <style type="text/css">
@@ -66,24 +68,24 @@
             <header>
                 <h1>Rechercher un capteur</h1>
                 <?php 
-                	include("dbconnect.php"); 
-	                include("nav.php");
+                	include("dbconnect.php"); //Database connection
+	                include("nav.php");	// Navbar
                 ?>
             </header>
-			<div class="ink-form large-100 medium-100 small-100">
+			<div class="ink-form column-group gutters large-100 medium-100 small-100">
 				<?php
-				// Check if the user is a valide user
+				// Check if the user is a validated user
 				if (isset($_COOKIE['login']) && isset($_COOKIE['password']) ){
 					$check=$bdd->prepare("SELECT login, password FROM users");
                 	$check->execute();
                 	$data=$check->fetch();
                 	$check->closeCursor();
                 	if($_COOKIE['login']==$data['login'] && $_COOKIE['password']==$data['password']){
-                	// -------------- If informations are requested -------------------------------
+                		// ---------------- If informations are requested ---------------
 						if (isset($_POST['id']) || isset($_POST['name'])){
 						  ?>
-						  	<!-- Show the informations --> 
-							<div class="ink-form large-100 medium-100 small-100">
+						  	<!-- Show the informations designed || Left column-->
+							<div class="ink-form large-50 medium-50 small-100">
 		    				<h3> Informations sur le capteur demandé </h3>
 			    			<table class="ink-table bordered">
 				    		<thead>
@@ -97,17 +99,20 @@
 						    </thead>                            
 						    <tbody>
 						  <?php
-						  	// If the informations are requested by the id
+						  	// Extract the informations from the database, searching by ID
 							if(isset($_POST['id'])){
 								$req = $bdd->prepare('SELECT data.id, data.name, captors.temp, captors.lum, captors.timestamp FROM data,captors WHERE data.id=? AND data.id=captors.id ORDER BY data.id');
 								$req->execute(array((int)$_POST['id']));
 							}
-							// If not, the informations are requested by name
+							// Extract the informations from the database, searching by name
 							else{
 								$req = $bdd->prepare('SELECT data.id, data.name, captors.temp, captors.lum, captors.timestamp FROM data,captors WHERE data.name=? AND data.id=captors.id ORDER BY data.name');
 								$req->execute(array($_POST['name']));
 							}
-								// Print the informations (id, name,temperature, lightning, dateof last update
+								// Display the extracted informations
+								$i=0;
+								$chart_temp; // Chart temperature array
+								$chart_lum; // Chart lightning array
 								while ($donnees = $req->fetch()){
 									echo '<tr>';
 								    echo '<td>' . '<center>' . $donnees['id'] . '</center>' . '</td>';
@@ -116,19 +121,73 @@
 									echo '<td>' . '<center>' . $donnees['lum'] . '</center>' . '</td>';
 									echo '<td>' . '<center>' . $donnees['timestamp'] . '</center>' . '</td>';
 									echo '</tr>';
+									$chart_lum[$i]=$donnees['lum']; // Save the lighning's informations for the chart
+									$chart_temp[$i]=$donnees['temp']; // Save the temperature's informations for the chart
+									$i++;
 								}
 								$req->closeCursor();
 				?>
 							</tbody>
 							</table>
 							</div>
+							<!-- Display the chart || Right column -->
+							<div class="ink-form large-50 medium-50 small-100">
+							<h4> <center>Graphique des 10 dernières mesures du capteur</center></h4>
+							<small><center>10 correspond à la valeur la plus récente</center></small>
+							<canvas id="chart" width="600" height="400">
+							</canvas>
+							<?php
+								for($i = 0;$i <=9;$i++){
+									if ($chart_lum[$i] == NULL)
+										$chart_lum[$i] = 0;
+									if ($chart_temp[$i] == NULL)
+										$chart_temp[$i] = 0;
+								}
+							?>
+							<!-- Javascript displaying the chart using Chart.js -->
+							<script type="text/javascript">
+							// Display the chart in "chart" div
+							var ctxChart = document.getElementById("chart").getContext("2d");
+							// Adding the data to the chart
+							var data = {
+								labels : ["1","2","3","4","5","6","7","8","9","10"],
+								datasets : [
+									{
+										fillColor : "rgba(255,140,0,0.5)",
+										strokeColor : "rgba(255,140,0,1)",
+										data : [<?php
+											for($i =0;$i <=8;$i++){
+												echo $chart_lum[$i].',';
+											}
+											echo $chart_lum[9];
+											echo ']';
+										?>
+									},
+									{
+										fillColor : "rgba(0,191,255,0.5)",
+										strokeColor : "rgba(0,191,255,1)",
+										data : [<?php
+											for($i =0;$i <=8;$i++){
+												echo $chart_temp[$i].',';
+											}
+											echo $chart_temp[9];
+											echo ']';
+										?>
+									}
+								]
+							}	
+							new Chart(ctxChart).Bar(data,{});
+							</script>
+							<!-- Caption -->
+							<center><small><font color="#FF8C00">Luminosité</font></small></br>
+							<small><font color="#00BFFF">Temperature</font></small></center>
+							</div>
+						</div>
 	           <?php }
-	           //---------------- End of informations request -------------------------
-	           
-	           //---------------- Show a basic page with requests choices ------------
+	           // -------------- If nothing is requested, display the search selector ------------
 	           	else {?>
+						<!-- By ID -->
 						<form action="search.php" method="post" class="ink-form">
-							<!-- Request by ID -->
 							<p>Recherche par ID : </p>
 							<select name="id">'
 							<fieldset class="column-group gutters">
@@ -136,7 +195,7 @@
 			    					<div class="control-group gutters required">
 				    					<div class="control medium-20">
 		    			<?php
-		    				// List all the captors by ID
+		    				// Extracts the informations from the DB and displays it
 		    				$value = $bdd->query('SELECT id FROM data ORDER BY id');
 		    				while ($data = $value->fetch()){
 		        				echo'<option value="'.$data['id'].'">'.$data['id'].'</option>';
@@ -150,8 +209,8 @@
 				    		</select>
 				    		<button type="submit" class="ink-button">Envoyer</button>
 				    	</form>
+				    	<!-- By name -->
 				    	<form action="search.php" method="post" class="ink-form">
-				    		<!-- Request by name -->
 							<p>Recherche par nom : </p>
 							<select name="name">'
 							<fieldset class="column-group gutters">
@@ -159,7 +218,7 @@
 			    					<div class="control-group gutters required">
 				    					<div class="control medium-20">
 		    			<?php
-		    				//list all the captors by name
+		    				// Extracts the informations from the DB and displays it
 		    				$value = $bdd->query('SELECT name FROM data ORDER BY name');
 		    				while ($data = $value->fetch()){
 		        				echo'<option value="'.$data['name'].'">'.$data['name'].'</option>';
@@ -174,7 +233,6 @@
 				    		<button type="submit" class="ink-button">Envoyer</button>
 				    	</form>
 			 <?php } 
-			 		// -----------------------End of choice page -------------------
 				 }
 			   }
 			    ?>
