239886ee311b3e3d742e3dcce04713da6ecf8091
benoit <maliarbenoit@msn.com>
2014-04-13 02:49:27 +0200

    Web Completed !
    Signed-off-by: benoit <maliarbenoit@msn.com>

--- Web/redirect.php
+++ Web/redirect.php
@@ -1,7 +1,7 @@
 <?php    
 /* ----------------- Redirection file used in case of login and logout -------------------
 						Case of login : Create a cookie with login and password given by POST. Timed for 1 hour.
-						Case of lougout : Set the previous cookie to destroyed state and set the time left to 0 (or negative time).
+						Case of logout : Set the previous cookie to destroyed state and set the time left to 0 (or negative time).
 -----------------------------------------------------------------------------------------*/  
   header('Location: index.php');
   if( isset($_POST['login']) ) setcookie('login', $_POST['login'], time() + 3600, null, null, false, true);
